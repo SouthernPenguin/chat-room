24237b2a2ffc93a6ed19cc299c006f07722e40f3
SouthernPenguin <940583551@qq.com>
2025-01-05 10:45:20 +0800

    refactor(custom): 单人聊天目录修改

--- app/dashboard/chatRoom/groupChat/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-import ChatContent from '@/app/components/ui/dashboard/chatRoom/SingleChatRoomContent';
-import React from 'react';
-
-const CharRoom = () => {
-  return <ChatContent />;
-};
-
-export default CharRoom;
