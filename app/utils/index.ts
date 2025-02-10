435d3e7093757af49e64f578e4fc72bb14366914
SouthernPenguin <940583551@qq.com>
2025-02-10 22:41:03 +0800

    perf(custom): localStorage 改为 sessionStorage

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -5,23 +5,22 @@ import moment from 'moment';
  * 模块名: 设置缓存
  * @param string token
  */
-export const setLocalStorage = (token: string, key: string = __TOKEN__) => localStorage.setItem(key, token);
+export const setLocalStorage = (token: string, key: string = __TOKEN__) => sessionStorage.setItem(key, token);
 
 /**
  * 模块名:获取缓存
  */
-export const getLocalStorage = (key: string = __TOKEN__) => localStorage.getItem(key);
+export const getLocalStorage = (key: string = __TOKEN__) => sessionStorage.getItem(key);
 
 /**
  * 模块名: 移除token
  */
-export const moveLocalStorageToken = () => localStorage.removeItem(__TOKEN__);
+export const moveLocalStorageToken = () => sessionStorage.removeItem(__TOKEN__);
 
 /**
  * 模块名:移除所有缓存
  */
 export const allLocalStorageMove = () => {
-  localStorage.clear();
   sessionStorage.clear();
 };
 
